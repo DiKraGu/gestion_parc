4ec6e0ee56ca80ab51f2e3c18fba32e7992eb9cc
DikraGuiri <dikra.guiri@gmail.com>
2025-04-28 00:06:07 +0100

    models admin

--- parc/admin.py
+++ parc/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Vehicule, Ticket
 
-# Register your models here.
+# Pour rendre ces modèles accessibles dans l'interface d'administration de Django, 
+# vous devez les enregistrer dans parc/admin.py. 
+# Cela permet à un administrateur de gérer facilement les objets User, Vehicule et Ticket 
+# directement à partir du tableau de bord d'administration de Django.
+
+admin.site.register(User)
+admin.site.register(Vehicule)
+admin.site.register(Ticket)
